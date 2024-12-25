dfd76b8ad177906e1f5b5acb9965ece57ba52cc3
dweight fuentes <dwieghtpro@gmail.com>
2024-12-26 00:05:08 +0800

    boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import ClientComponent from "~/components/ClientComponent";
 import ServerComponent from "~/components/ServerComponent";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center w-full h-screen gap-y-5">
-      <h1 className="font-bold text-xl">Welcome to Magaaazine.</h1>
-      <ClientComponent />
-      <ServerComponent />
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-center w-full h-screen gap-y-5">
+            <h1 className="font-bold text-xl">Welcome to Magaaazinekie.</h1>
+            <ClientComponent />
+            <ServerComponent />
+        </main>
+    );
 }
